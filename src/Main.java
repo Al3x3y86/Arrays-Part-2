efaefeb41d09af29cf66a1e74f0e6f88cd095663
Alexey <alexey_kuzminykh@mail.ru>
2022-10-19 02:45:36 +0300

    Arrays, task 3

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int[] arr=new int[30];
-        for(int i=0;i<arr.length;i++){
-            arr[i]=(int)(Math.random()*100_000 + 100_000);
+        int[] arr = new int[30];
+        double average = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100_000 + 100_000);
+            average += arr[i] / arr.length;
             }
-        int min, max = min = arr[0];
 
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
 
-        for (int i : arr) {
-            min = Math.min(min,i);
-            max = Math.max(max,i);
         }
-        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей");
     }
-}
